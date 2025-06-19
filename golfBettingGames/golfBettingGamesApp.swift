48562545f40868f887f2d5a6be989d6c31e7203e
William <wcastellano13@gmail.com>
2025-06-19 09:54:41 -0400

    Initial Commit

---
+++ golfBettingGames/golfBettingGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  golfBettingGamesApp.swift
+//  golfBettingGames
+//
+//  Created by William Castellano on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct golfBettingGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
